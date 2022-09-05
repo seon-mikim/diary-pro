1f287d8e017393dbc47aacd679d740e693fca7f4
Seon Mi Kim <tjsalsun@gmail.com>
2022-09-06 02:49:46 +0900

    Update Edit.jsx

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { DiaryDispatchContext } from '../App'
 import Header from '../components/Header'
 import Layout from '../components/Layout'
 
 const Edit = () => {
+  
+  const [originData, setOriginData] = useState()
+  const navigate = useNavigate()
+  const {id} = useParams()
+  
+  const diaryList = useContext(DiaryDispatchContext)
+
+  useEffect(()=>{
+    if(diaryList.length >= 1){
+        const targetDiary = diaryList.find(
+          (it)=>parseInt(it.id) === parseInt(id)
+          );
+          console.log(targetDiary)
+
+          if(targetDiary){
+
+          }else{
+            navigate('/',{replace: true})
+          }
+    }
+  },[id, diaryList])
+
+  console.log(id)
   return (
     <Layout>
     <Header/>  
