885f5d76f64f9f1b175608fffde69c6528139fa5
xiuxiubok <tjsalsun@gmail.com>
2022-09-06 07:58:53 +0900

    emotion.js 파일 정보 불러오기

--- src/components/DiaryEditor.jsx
+++ src/components/DiaryEditor.jsx
@@ -5,36 +5,11 @@ import Header from './Header'
 import { useNavigate } from 'react-router-dom'
 import {DiaryDispatchContext} from '../App'
 import { getStringDate } from '../util/data'
+import { emotionList } from '../util/emotion'
 import Btn from './Btn'
 import EmotionItem from './EmotionItem'
 
-const emotionList = [
-    {
-        emotion_id: 1,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-        emotion_descript:'완전좋음'
-    },
-    {
-        emotion_id: 2,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-        emotion_descript:'좋음'
-    },
-    {
-        emotion_id: 3,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-        emotion_descript:'그럭저럭'
-    },
-    {
-        emotion_id: 4,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-        emotion_descript:'나쁨'
-    },
-    {
-        emotion_id: 5,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-        emotion_descript:'완전나쁨'
-    },
-]
+
 
 const DiaryEditor = ({isEdit, originData}) => {
     const contentRef = useRef()
